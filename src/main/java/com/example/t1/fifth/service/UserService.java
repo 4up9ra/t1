add025028fb22cea7f8d6496285083ff707532c1
kirill <4up9ra@gmail.com>
2025-08-06 09:28:18 +0300

    hw5

---
+++ src/main/java/com/example/t1/fifth/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.t1.fifth.service;
+
+import com.example.t1.fifth.repository.UserRepository;
+import com.example.t1.fifth.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User create(String username) {
+        User user = new User();
+        user.setUsername(username);
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteByUsername(String username) {
+        userRepository.deleteByUsername(username);
+    }
+}
